950012e7bea10a6bc00babb9e03b24a6f0acf9f5
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-11-17 14:56:16 +0000

    stampa iterator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <set>
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
+    set<int> si;
+    si.insert(5);
+    si.insert(2);
+    si.insert(9);
+    
+    set<int>::iterator iter;
+    for(iter = si.begin(); iter != si.end(); ++iter){
+        cout << *iter << endl;
+    }
+
+    cout << "--------" << endl;
+
+    set<int>::const_iterator citer;
+    for(citer = si.cbegin(); citer != si.cend(); ++citer){
+        cout << *citer << endl;
+    }
+
+    cout << "--------" << endl;
+
+    set<int>::reverse_iterator riter;
+    for(riter = si.rbegin(); riter != si.rend(); ++riter){
+        cout << *riter << endl;
+    }
+
+    cout << "--------" << endl;
+
 } 
